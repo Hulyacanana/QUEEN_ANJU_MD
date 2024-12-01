a4456b048b78981fea92d5b9fd2a98ce0332b312
Hulyacanana <158591864+Hulyacanana@users.noreply.github.com>
2024-12-01 18:44:12 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=eYs1yJDQ#Y3v5mocu8uLKx4Y1Lu_4rCWZsTPvRB7XRw3FdjEX6iw",
 };
