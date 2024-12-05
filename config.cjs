3737839db6284d219a0f64c809c9c0db8c5a7af4
Hulyacanana <158591864+Hulyacanana@users.noreply.github.com>
2024-12-06 00:07:52 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   "ALIVE_IMG": "https://raw.githubusercontent.com/RASH-DATA/ANJU-DATA/refs/heads/main/WhatsApp%20Image%202024-09-08%20at%209.00.17%20PM.jpeg",
-  "PREFIX": ".",
+  "PREFIX": "",
   "AUTO_READ_STATUS": true,
   "MODE": "inbox",
   "BOT_NUMBER": "94704276051",
